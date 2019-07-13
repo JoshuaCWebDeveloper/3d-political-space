2150beccba49053e14dd137898d15c711fe0544d
JoshuaCWebDeveloper <coosriverjoshua1@outlook.com>
2019-07-13 11:56:07 -0700

    Parse params and render new PoliticalSpace component in app.js

--- app/app.js
+++ app/app.js
@@ -1,25 +1,20 @@
 /* app.js
  * Main 3D Political Space app file
  * Dependencies: 
-    - modules: query-string, react, react-dom, three, whs
-    - classes: Cube, Location, ThreeOrbitControls
+    - modules: query-string, react, react-dom
+    - components: PoliticalSpace
  * Author: Joshua Carter
  * Created: November 11, 2018
  */
 "use strict";
-
 //import modules
 import qs from 'query-string';
 import ReactDom from 'react-dom';
 import React from 'react';
-import * as THREE from 'three';
-import * as WHS from 'whs';
-//include classes
-import { Cube } from './Cube.js';
-import { Location } from './Location.js';
-import { ThreeOrbitControls } from './ThreeOrbitControls.js';
+//include components
+import { PoliticalSpace } from './PoliticalSpace.js';
 
-//parse params
+//fetch params
 const params = Object.assign({
     title: '',
     location: "",
@@ -29,7 +24,13 @@ const params = Object.assign({
 if (params.vectors && !Array.isArray(params.vectors)) {
     params.vectors = [params.vectors];
 }
-
+//parse
+params.location = params.location ? params.location.split(",")
+    .map(it => parseFloat(it)) : [];
+params.vectors = params.vectors ? params.vectors.map(
+    it => it.split(",").map(it => parseFloat(it))
+) : [];
+        
 //render HTML
 ReactDom.render(
     (
@@ -38,7 +39,7 @@ ReactDom.render(
                 <h1>{params.title}</h1>
             </div>
         
-            <div id="app-container"></div>
+            <PoliticalSpace location={params.location} vectors={params.vectors} />
         </div>
     ), document.getElementById('view-container')
 );
